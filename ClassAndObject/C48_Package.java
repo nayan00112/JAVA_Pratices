668c1d4cc04c4003ed5ffacefa060657106e40b3
Nayan Patel <nayanpatel00112@gmail.com>
2023-03-28 22:16:58 +0530

    java

---
+++ ClassAndObject/C48_Package.java
@@ -0,0 +1,16 @@
+/* Package
+ * Domain Name: google.com
+ * Package name: com.google {Unique name...}
+ * Package name should be mirror of domain name.
+ * 
+ * It is batter to put class into package for maintain classes.
+ * same name file can exist there, due to different packages. (in Eclipse)
+*/
+
+// package ClassAndObject2; // Error...!!! (why?)
+public class C48_Package {
+    public static void main(String arg[])
+    {
+        System.out.println("Hello World");
+    }
+}

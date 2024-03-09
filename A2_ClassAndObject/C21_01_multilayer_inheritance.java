7f83716ac526a14e8eb5d311555ddf8f31430f2d
Nayan Patel <nayanpatel00112@gmail.com>
2024-03-09 10:04:53 +0530

    multilayer inheritance

--- A2_ClassAndObject/C21_01_multilayer_inheritance.java
+++ A2_ClassAndObject/C21_01_multilayer_inheritance.java
@@ -26,3 +26,73 @@ class C21_01_multilayer_inheritance {
 // in C super super
 // in B super
 // in a
+
+
+
+/*
+In Python, if a subclass has its own `__init__` method and does not 
+explicitly call the `__init__` method of its parent class using `super().
+__init__()`, the constructor of the parent class won't be automatically 
+called. 
+
+
+In your Python example:
+
+```python
+class B:
+    def __init__(self):
+        print("in B super")
+
+class A(B):
+    def __init__(self):
+        print("in A")
+
+aa = A()
+```
+
+The output will be:
+
+```
+in A
+```
+
+This is because the `__init__` method of class `A` does not call the 
+`__init__` method of its parent class `B`.
+
+On the other hand, in your Java example:
+
+```java
+class B {
+    public B() {
+        System.out.println("in B super");
+    }
+}
+
+class A extends B {
+    public A() {
+        System.out.println("in a");
+    }
+}
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        A aa = new A();
+    }
+}
+```
+
+The output will be:
+
+```
+in B super
+in a
+```
+
+In Java, when you create an instance of class `A`, the constructor of class 
+`B` is automatically called (unless the parent class has a parameterized 
+constructor and you need to explicitly call it using `super(...)`). Java 
+handles the constructor chaining differently than Python. In Python, you 
+need to explicitly call `super().__init__()` if you want to invoke the 
+constructor of the parent class.
+
+*/

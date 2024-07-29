55ef2039b5f3f4c70695f355f69263e3f2df1e6d
Nayan Patel <nayanpatel00112@gmail.com>
2024-07-29 22:27:56 +0530

    final keyword

--- A2_ClassAndObject/C34_FinalKeyword_Variable.java
+++ A2_ClassAndObject/C34_FinalKeyword_Variable.java
@@ -6,6 +6,37 @@
 */
 
 
+// Final Variable:
+
+// Declared using the final modifier before the variable name.
+// Cannot be modified after initialization.
+// Guarantees that the value of the variable will remain constant throughout the program’s execution.
+// Final Method:
+
+// Declared using the final modifier before the method name.
+// Cannot be overridden by subclasses.
+// Ensures that the implementation of the method will not change in subclasses.
+// Final Class:
+
+// Declared using the final modifier before the class name.
+// Cannot be extended by other classes.
+// Prevents the creation of subclasses and ensures that the class’s implementation is final.
+// Example:
+
+// // Final variable
+// final int age = 30;
+
+// // Final method
+// final void display() {
+//     System.out.println("Final method");
+// }
+
+// // Final class
+// final class MyClass {
+//     // No subclasses can be created
+// }
+
+
 // >>>>>>>>>>>>>>>>>>>>>>>>>>> Final Variable <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 class A{
     final int Day = 5; // constant
